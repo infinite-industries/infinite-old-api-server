b1a6322d9e63cb9dee2c2e9f581addf491d2f62c
chriswininger <cwininger@airspringsoftware.com>
2017-10-08 20:26:16 -0400

    more fix

--- routes/events.js
+++ routes/events.js
@@ -4,13 +4,13 @@ const { getDefaultRouter } = require("./routeHelpers");
 const router = getDefaultRouter("events", "event", EventController);
 const uuidv1 = require('uuid/v1');
 
-router.use('/'), function(req, res, next) {
+router.use('/', function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Headers',
 		'Content-Type, Authorization, Content-Length, X-Requested-With');
 	next();
-}
+});
 
 router.post('/', function(req, res) {
 	const eventJSON= req.body.event;
