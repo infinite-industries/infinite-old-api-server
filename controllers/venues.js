48f7c720758b5f0d7caafd727a6ebf7fe0daa8b3
chriswininger <cwininger@airspringsoftware.com>
2018-09-02 10:39:50 -0400

    add an enpoint that fetches or creates a venue

--- controllers/venues.js
+++ controllers/venues.js
@@ -1,2 +1,14 @@
 const getDefaultController = require('./helpers/controllerGenerator');
-module.exports = getDefaultController('venue');
+const controller = getDefaultController('venue')
+module.exports = {
+  ...controller,
+  ensureExistsByName: (db, venue, callback) => {
+    db.venue.find({ name: venue.name }).then((foundVenue) => {
+      if (foundVenue) {
+        callback(null, foundVenue);
+      } else {
+        controller.create(db, venue, callback)
+      }
+    }).catch(err => callback(err))
+  }
+};
--- routes/venues.js
+++ routes/venues.js
@@ -3,4 +3,19 @@ const VenueController = require("../controllers/venues");
 const { getDefaultRouter } = require("./helpers/routeHelpers");
 const router = getDefaultRouter("venues", "venue", VenueController);
 
+router.post('/ensure', JWTAuthChain, (req, res) => {
+  const postJSON= req.body.venue;
+  if (!postJSON) {
+    return res.status(400).json({ status: 'missing venue attribute' })
+  }
+
+  VenueController.ensureExistsByName(req.app.get('db'), postJSON, (err, venue) => {
+    if (err) {
+      res.status(500).json({ status: err });
+    } else {
+      res.status(200).json({ status: 'ok', venue })
+    }
+  })
+})
+
 module.exports = router;
