cf3caedda22bc8a655f5fb3e4a2a2bec01f927e9
dima <shifting.planes@gmail.com>
2018-12-06 08:43:18 -0500

    added migrations to deal with early event expiration

--- deploy.sh
+++ deploy.sh
@@ -6,26 +6,43 @@ USER='ubuntu'
 SERVER=''
 if [[ "production" = $1 ]]; then
   SERVER='api.infinite.industries'
+
+  ssh $USER@$SERVER bash --login -i << EOF
+    cd $ROOT
+    echo 'Updating sources'
+    git reset --hard HEAD
+    git checkout master
+    git pull origin master
+    echo 'Installing npm packages'
+    npm install
+    echo 'Restarting'
+    forever stop infinite
+    rm /home/$USER/.forever/infinite.log
+    forever start --uid infinite index.js
+    echo 'Done!'
+EOF
+
 elif [[ "staging" = $1 ]]; then
   SERVER='staging-api.infinite.industries'  #not used yet
+
+
+  ssh $USER@$SERVER bash --login -i << EOF
+    cd $ROOT
+    echo 'Updating sources'
+    git reset --hard HEAD
+    git checkout master
+    git pull origin master
+    echo 'Installing npm packages'
+    npm install
+    echo 'Restarting'
+    forever stop infinite
+    rm /home/$USER/.forever/infinite.log
+    forever start --uid infinite index.js
+    echo 'Done!'
+EOF
 else
   echo Please specify environment to deploy to.
   echo Usage: ./deploy.sh environment
   echo Example: ./deploy.sh staging
   exit
 fi
-
-ssh $USER@$SERVER bash --login -i << EOF
-  cd $ROOT
-  echo 'Updating sources'
-  git reset --hard HEAD
-  git checkout master
-  git pull origin master
-  echo 'Installing npm packages'
-  npm install
-  echo 'Restarting'
-  forever stop infinite
-  rm /home/$USER/.forever/infinite.log
-  forever start --uid infinite index.js
-  echo 'Done!'
-EOF
